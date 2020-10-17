af779d1f8c9472152b23f8504a04bfb42fd3cd20
safesparrow <janusz.wrobel.2@gmail.com>
2020-10-17 22:31:12 +0100

    initial

---
+++ src/Firebase.js
@@ -0,0 +1,13 @@
+﻿import * as firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBXcQj3MO6xXtMA3-sbT59n7i4Gdk_lx7o",
+    authDomain: "groceries-toolkit.firebaseapp.com",
+    databaseURL: "https://groceries-toolkit.firebaseio.com",
+    projectId: "groceries-toolkit",
+    storageBucket: "groceries-toolkit.appspot.com",
+    messagingSenderId: "729857865421",
+    appId: "1:729857865421:web:bafdff6494d36bba5ddb84"
+};
+firebase.initializeApp(firebaseConfig)
+export const db = firebase.database()
