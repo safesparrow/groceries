c8878093c25d43a621248106375b81b465f0c5bd
safesparrow <janusz.wrobel.2@gmail.com>
2020-12-13 17:23:11 +0000

    More navigation in recipes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {FormEvent, useState} from 'react'
-import {BrowserRouter, Route, Switch, Link, useParams, useRouteMatch} from 'react-router-dom'
+import {Redirect, BrowserRouter, Route, Switch, Link, useParams, useRouteMatch} from 'react-router-dom'
 import './App.css';
 import './Firebase';
 import {Button, Col, Form, Nav, Spinner, Table, Tabs} from 'react-bootstrap'
@@ -63,9 +63,11 @@ function RecipeForm(props: { onSubmit: (r: SimpleRecipe) => void }) {
 
 function Recipies() {
 
-    const [_recipes, loading, _]: [SimpleRecipe[] | undefined, boolean, unknown] = useListVals<SimpleRecipe>(recipesRef);
+    const [_recipes, loading, _]: [Record<string, SimpleRecipe> | undefined, boolean, unknown] = useObjectVal<Record<string, SimpleRecipe>>(recipesRef);
     const recipes = _recipes || []
-
+    
+    const { url, path } = useRouteMatch();
+    
     function onAdd(recipe: SimpleRecipe) {
         let ref = recipesRef.push()
         recipe.id = ref.key!
@@ -77,23 +79,29 @@ function Recipies() {
     function handleRemove(toRemove: SimpleRecipe) {
         recipesRef.child(toRemove.id).remove()
     }
+    console.log(url, path)
 
-    return <>
-        {loading ? <Spinner animation='border'/>
-            :
-            <Table striped bordered hover>
-                <tbody>
-                {recipes.map(p =>
-                    <tr key={p.id}>
-                        <td>{p.title}</td>
-                        <td><Button variant='outline-warning' onClick={() => handleRemove(p)}>Remove</Button></td>
-                    </tr>
-                )}
-                </tbody>
-            </Table>
-        }
-        <RecipeForm onSubmit={onAdd}/>
-    </>
+    return <Switch>
+        <Route path={`${path}/`} exact>
+            <Link to={`${url}/new`}><Button variant={'outline-info'}>Add a recipe</Button></Link>
+            {loading ? <Spinner animation='border'/>
+                :
+                <Table striped bordered hover>
+                    <tbody>
+                    {Object.entries(recipes).map(([k,p]) =>
+                        <tr key={p.id}>
+                            <td>{p.title}</td>
+                            <td><Button  variant='outline-warning' onClick={() => handleRemove(p)}>Remove</Button></td>
+                        </tr>
+                    )}
+                    </tbody>
+                </Table>
+            }
+        </Route>
+        <Route path={`${path}/new`} exact>
+            <RecipeForm onSubmit={onAdd}/>
+        </Route>
+    </Switch>
 }
 
 function App() {
@@ -104,27 +112,17 @@ function App() {
                 // onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
             >
                 <Nav.Item>
-                    <Nav.Link as={Link} eventKey='active' to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} eventKey="recipes" to='/recipes'>Recipes</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link as={Link} eventKey="recipes" to='/recipes'>Recipes</Nav.Link>
+                    <Nav.Link as={Link} eventKey="products" to='/products'>Products</Nav.Link>
                 </Nav.Item>
             </Nav>
             <div>
                 <Switch>
-                    <Route path='/' exact>
-                        <Tabs>
-                            <Tab eventKey='recipies' title='Recipies'>
-                                <Recipies/>
-                            </Tab>
-                            <Tab eventKey='products' title='Products'>
-                                <ProductsManager/>
-                            </Tab>
-                        </Tabs>
-                    </Route>
-                    <Route path='/recipes'>
-                        <Recipies/>
-                    </Route>
+                    <Route path='/products'><ProductsManager /></Route>
+                    <Route path={['/recipes']}><Recipies/></Route>
+                    <Redirect to={{pathname: '/recipes'}} />
                 </Switch>
             </div>
         </BrowserRouter>
